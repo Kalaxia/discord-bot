63030852df6e7b453f78663054b0641923772ff0
Yir <noltheo03@gmail.com>
2018-04-03 19:05:44 +0200

    added error reason into json response & change payload type to support float and integers

--- controller/poll_controller.go
+++ controller/poll_controller.go
@@ -5,22 +5,31 @@ import (
 	"encoding/json"
 	"net/http"
 	"discord-bot/utils"
+	"strconv"
 	"bytes"
 	"log"
 )
 
 func NewPollAction(session *discordgo.Session, pollChannel string, writer http.ResponseWriter, request *http.Request) {
-	var payload map[string]string
+	var payload map[string]interface{}
 
 	decoder := json.NewDecoder(request.Body)
 	if err := decoder.Decode(&payload); err != nil {
 		log.Println(err)
-		utils.BuildJsonResponse("error", writer)
+		utils.BuildJsonResponse("error", "payload decoding error", writer)
 		return
 	}
 
+	if !utils.CheckKey(payload, "id") {
+		utils.BuildJsonResponse("error", "invalid payload", writer)
+		return
+	}
+
+	// Convert parameters
+	pollId := strconv.FormatFloat(payload["id"].(float64), 'f', 0, 64)
+
 	// Build response
-	pollUrl := "https://www.kalaxia.com/polls/" + payload["id"]
+	pollUrl := "https://www.kalaxia.com/polls/" + pollId
 
 	var response bytes.Buffer
 	response.WriteString("@everyone\n**Un nouveau vote à été soumis ! Allez voter sur ")
@@ -31,10 +40,10 @@ func NewPollAction(session *discordgo.Session, pollChannel string, writer http.R
 	_, err := session.ChannelMessageSend(pollChannel, response.String())
 	if err != nil {
 		log.Println(err)
-		utils.BuildJsonResponse("error", writer)
+		utils.BuildJsonResponse("error", "discord send message error", writer)
 		return
 	}
 
 	// Send http response
-	utils.BuildJsonResponse("ok", writer)
+	utils.BuildJsonResponse("ok", "notification sent", writer)
 }
--- utils/http.go
+++ utils/http.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-func BuildJsonResponse(status string, writer http.ResponseWriter) {
-	response, _ := json.Marshal(map[string]string{"status": status})
+func BuildJsonResponse(status string, reason string, writer http.ResponseWriter) {
+	response, _ := json.Marshal(map[string]string{"status": status, "reason": reason})
 
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(200)
