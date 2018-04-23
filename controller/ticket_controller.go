e1c785961f365a8acd443a59e53729ceee72f569
Yir <noltheo03@gmail.com>
2018-04-23 11:59:01 +0200

    removed brackets arround url

--- controller/ticket_controller.go
+++ controller/ticket_controller.go
@@ -57,7 +57,7 @@ func AssignToTicketAction(writer http.ResponseWriter, request *http.Request) {
 	server.SendDiscordMessage(
 		"board",
 		"**" + payload["developer"].(string) + "** a été assigné sur la carte **" + payload["title"].(string) + "**" +
-		" (https://www.kalaxia.com/feedbacks/" + payload["slug"].(string) + ") :construction_site:",
+		" https://www.kalaxia.com/feedbacks/" + payload["slug"].(string) + " :construction_site:",
 	)
 	utils.SendResponse(writer, 204, "")
 }
@@ -71,7 +71,7 @@ func ValidateTicketAction(writer http.ResponseWriter, request *http.Request) {
 	server.SendDiscordMessage(
 		"board",
 		"**" + payload["tester"].(string) + "** a validé la carte **" + payload["title"].(string) + "**" +
-		" (https://www.kalaxia.com/feedbacks/" + payload["slug"].(string) + ") :white_check_mark:",
+		" https://www.kalaxia.com/feedbacks/" + payload["slug"].(string) + " :white_check_mark:",
 	)
 	utils.SendResponse(writer, 204, "")	
 }
@@ -85,7 +85,7 @@ func CommentTicketAction(writer http.ResponseWriter, request *http.Request) {
 	server.SendDiscordMessage(
 		"board",
 		"**" + payload["author"].(string) + "** a commenté la carte **" + payload["title"].(string) + "**" +
-		" (https://www.kalaxia.com/feedbacks/" + payload["slug"].(string) + ") :writing_hand:",
+		" https://www.kalaxia.com/feedbacks/" + payload["slug"].(string) + " :writing_hand:",
 	)
 	utils.SendResponse(writer, 204, "")
 }
